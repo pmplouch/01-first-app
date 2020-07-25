a1cd56f02ae2b2f85e58e9ed8fea963565a97b4a
irconde <irconde@ualr.edu>
2020-07-25 11:58:19 -0500

    MainActivity members replaced by view binding reference

--- app/src/main/java/com/ualr/helloworld/MainActivity.java
+++ app/src/main/java/com/ualr/helloworld/MainActivity.java
@@ -2,25 +2,30 @@ package com.ualr.helloworld;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ualr.helloworld.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView userMsgTV;
-    private EditText userInputET;
+    // TODO 02. We replace members referring to GUI elements with a member of the new Binding class
+    //  automatically created
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        userMsgTV = findViewById(R.id.userMsgTV);
-        userInputET = findViewById(R.id.userInputET);
+        // TODO 03. Initialize the binding reference using the inflate method of the Binding class
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        // TODO 04. Get a reference to the root view
+        View rootView = mBinding.getRoot();
+        // TODO 05. Make the view active to display it on the screen
+        setContentView(rootView);
     }
 
     public void showTextMessage(View view) {
-        userMsgTV.setText(userInputET.getText().toString());
+        // TODO 06. We use the binding reference to access the GUI elements and get/modify their properties
+        mBinding.userMsgTV.setText(mBinding.userInputET.getText().toString());
     }
 }
